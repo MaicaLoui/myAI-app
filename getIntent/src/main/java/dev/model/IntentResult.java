09067ceb47713fa1bbc685cde1a72933a0da78cf
Maica Loui <90541475+MaicaLoui@users.noreply.github.com>
2022-05-26 00:37:32 -0400

    Add files via upload

---
+++ getIntent/src/main/java/dev/model/IntentResult.java
@@ -0,0 +1,35 @@
+package dev.model;
+
+public class IntentResult {
+
+    private String result;
+    private String note;
+    private String table;
+
+    public IntentResult() {
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+}
